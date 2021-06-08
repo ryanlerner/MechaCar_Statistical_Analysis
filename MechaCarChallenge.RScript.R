777c5e3a53e7beb0324e20566710c2acbda4bd08
ryanlerner <77894711+ryanlerner@users.noreply.github.com>
2021-06-08 14:27:12 -0400

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -8,3 +8,14 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 susp_coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
 total_summary <- susp_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# T-Tests on Suspension Coils
+t.test(susp_coil$PSI, mu = 1500)
+
+Lot1 = subset(susp_coil, Manufacturing_Lot == 'Lot1')
+Lot2 = subset(susp_coil, Manufacturing_Lot == 'Lot2')
+Lot3 = subset(susp_coil, Manufacturing_Lot == 'Lot3')
+
+t.test(Lot1$PSI, mu = 1500)
+t.test(Lot2$PSI, mu = 1500)
+t.test(Lot3$PSI, mu = 1500)
